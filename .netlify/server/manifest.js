c03a323c9ee70903d62f3eb76d67dceb42452bfd
slime103 <zes888@gmail.com>
2024-06-10 22:42:03 -0400

    Initial commit

---
+++ .netlify/server/manifest.js
@@ -0,0 +1,35 @@
+export const manifest = (() => {
+function __memo(fn) {
+	let value;
+	return () => value ??= (value = fn());
+}
+
+return {
+	appDir: "_app",
+	appPath: "_app",
+	assets: new Set(["favicon.png"]),
+	mimeTypes: {".png":"image/png"},
+	_: {
+		client: {"start":"_app/immutable/entry/start.CZNQxNhF.js","app":"_app/immutable/entry/app.C8iH3rhH.js","imports":["_app/immutable/entry/start.CZNQxNhF.js","_app/immutable/chunks/entry.GV0wMAo8.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/entry/app.C8iH3rhH.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.D6A44wRX.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		nodes: [
+			__memo(() => import('./nodes/0.js')),
+			__memo(() => import('./nodes/1.js')),
+			__memo(() => import('./nodes/2.js'))
+		],
+		routes: [
+			{
+				id: "/",
+				pattern: /^\/$/,
+				params: [],
+				page: { layouts: [0,], errors: [1,], leaf: 2 },
+				endpoint: null
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		},
+		server_assets: {}
+	}
+}
+})();
